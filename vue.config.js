ed0186b2ba56d1b418cdf06166d25b78b8ceda32
Stephane Busso <stephane.busso@gmail.com>
2019-03-24 06:00:19 +0700

    update dev

--- vue.config.js
+++ vue.config.js
@@ -2,19 +2,26 @@ const path = require('path')
 const PrerenderSPAPlugin = require('prerender-spa-plugin')
 
 module.exports = {
+  devServer: {
+    open: process.platform === 'darwin',
+    host: '0.0.0.0',
+    port: 8080, // CHANGE YOUR PORT HERE!
+    https: false,
+    hotOnly: false
+  },
   configureWebpack: () => {
-    if (process.env.NODE_ENV !== 'production') return;
+    if (process.env.NODE_ENV !== 'production') return
     return {
       plugins: [
         new PrerenderSPAPlugin(
           // Absolute path to compiled SPA
           path.resolve(__dirname, 'dist'),
           // List of routes to prerender
-          [ '/'],
+          ['/'],
           {
             // options
           }
-        ),
+        )
       ]
     }
   }
